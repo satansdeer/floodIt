3aa9e39e8af248aa1a2b464270a262336e483f9b
Maxim Ivanov <satansdeer@gmail.com>
2013-07-13 00:23:52 +0400

    fixed color panel

--- FloodIt/ColorPanel.m
+++ FloodIt/ColorPanel.m
@@ -16,10 +16,13 @@
 	if( (self=[super init]) ) {
         self.myDelegate = delegate;
         CGSize winSize = [[CCDirector sharedDirector] winSize];
+        float tileSize = (winSize.width-8-(2*6))/6;
         self.items = [[NSMutableArray alloc] init];
         int i = 0;
         for (NSString*color in colors) {
+            
             FloodItem*item = [[FloodItem alloc] initWithFile:color position:CGPointMake(0, 0) andDelegate:self];
+            item.scale = tileSize/item.boundingBox.size.width;
             item.anchorPoint = CGPointMake(0, 0);
             float totalSize = colors.count*(item.boundingBox.size.width+2);
             item.position = CGPointMake(i*(item.boundingBox.size.width+2) + (winSize.width/2 - totalSize/2), 8);
